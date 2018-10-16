a0fe96990b0ec8c7f95ff2b4ad5b0e0cd8faab08
Sofia Berglund <berglund.sofia@gmail.com>
2018-10-16 15:02:39 +0200

    Updates

--- precache-manifest.83558be568faa7cc84c8281c50a19e26.js
+++ precache-manifest.83558be568faa7cc84c8281c50a19e26.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/Portfolio/static/js/runtime~main.3b961160.js"
   },
   {
-    "revision": "94277eda2e7d3b797d6e",
-    "url": "/Portfolio/static/js/main.94277eda.chunk.js"
+    "revision": "c3fddb7ee7967ca64a88",
+    "url": "/Portfolio/static/js/main.c3fddb7e.chunk.js"
   },
   {
     "revision": "05d4fdf7230935f7d686",
     "url": "/Portfolio/static/js/1.05d4fdf7.chunk.js"
   },
   {
-    "revision": "94277eda2e7d3b797d6e",
-    "url": "/Portfolio/static/css/main.6d82ea2d.chunk.css"
+    "revision": "c3fddb7ee7967ca64a88",
+    "url": "/Portfolio/static/css/main.3da8a3e6.chunk.css"
   },
   {
-    "revision": "4cc43614cecfb5842c305b025cf1f8d5",
+    "revision": "952afb6367e2b59c5621cdd32e24d7be",
     "url": "/Portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.031add24347a20a1a93ea97e1be23b2c.js"
+  "/Portfolio/precache-manifest.83558be568faa7cc84c8281c50a19e26.js"
 );
 
 workbox.clientsClaim();
