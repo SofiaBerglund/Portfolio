65d73072c55793fdcc1f57c3abeec121fb128336
Sofia Berglund <berglund.sofia@gmail.com>
2018-10-25 17:57:30 +0200

    Updates

---
+++ precache-manifest.1a29ebd045d26240c9da44f5df44a1af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "29eda360fe2bd2e56efb",
+    "url": "/Portfolio/static/js/main.29eda360.chunk.js"
+  },
+  {
+    "revision": "7981c65d057430c94415",
+    "url": "/Portfolio/static/js/1.7981c65d.chunk.js"
+  },
+  {
+    "revision": "29eda360fe2bd2e56efb",
+    "url": "/Portfolio/static/css/main.cecce623.chunk.css"
+  },
+  {
+    "revision": "b463d53b516d659915d8b147dbd1e624",
+    "url": "/Portfolio/index.html"
+  }
+];
